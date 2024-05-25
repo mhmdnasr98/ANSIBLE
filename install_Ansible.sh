1508be2f8dc1bbba4e76ad7c0ce9a95888bcc49f
mhmdnasr98 <167673412+mhmdnasr98@users.noreply.github.com>
2024-05-25 16:45:11 +0300

    Update install_Ansible.sh

--- install_Ansible.sh
+++ install_Ansible.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 exec > >(tee -i /var/log/user-data.log)
 exec 2>&1
+
+# Update apt package lists
 sudo apt update -y
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible -y
-sudo apt install git -y 
-mkdir Ansible && cd Ansible
-pwd
-git clone https://github.com/mhmdnasr98/ANSIBLE.git
-cd ANSIBLE
+
+# Install required software
+sudo apt install -y software-properties-common
+sudo apt install -y ansible git
+
+# Clone the Ansible playbook repository
+mkdir Ansible
+cd Ansible
+git clone https://github.com/mhmdnasr98/installitions.git
+
+# Run Ansible playbook
+cd installitions
 ansible-playbook -i localhost Jenkins-playbook.yml
